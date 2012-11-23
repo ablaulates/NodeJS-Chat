f134766a071d31e2cecf7deb5f332d13de33a320
Guilherme Farias <guilherme@guiky.com.br>
2012-11-23 17:32:14 -0300

    remodelando o chat e preparando para adição de salas

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-var fs = require('fs'),
-	http = require('http'),
-	socketio = require('socket.io');
- 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end(fs.readFileSync(__dirname + '/index.html'));
-}).listen(8080, function() {
-    console.log('Listening at: http://localhost:8080');
-});
- 
-socketio.listen(server).on('connection', function (socket) {
-	socket.on('set nickname', function (name) {
-		socket.set('nickname', name, function () {
-			socket.emit('ready');
-		});
+var room = 1,
+	users = {},
+	fs = require('fs'),
+	io = require('socket.io').listen(4000);
+
+io.sockets.on('connection', function (socket) {
+	var id;
+
+	socket.on('set user', function (user) {
+		id = user.user_id;
+		users[id] = user.user_name;
+		socket.join('room-' + room);
 	});
 
 	socket.on('message', function (msg) {
-		socket.get('nickname', function (err, name) {
-			var message = name + ' diz: ' + msg;
-			socket.broadcast.emit('message', message);
-		});
+		var message = {
+			sender_id: id,
+			sender_name: users[id],
+			message: msg
+		};
+		io.sockets.in('room-' + room).emit('message', message);
+	});
+
+	socket.on('disconnect', function () {
+		delete users[id];
 	});
 });
